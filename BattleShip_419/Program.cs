1c7c4134a97250aa8942fac5cc43a5e182ea5ed5
Pamy Ann Patrick <pamyannpatrick@gmail.com>
2022-12-28 22:19:08 -0500

    initial changes

---
+++ BattleShip_419/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip_419
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            char[][] board =  new char[][] { 
+                new char[] { 'X', '.', '.', 'X' },
+                new char[] { '.', '.', '.', 'X' },
+                new char[] { 'X', '.', '.', 'X' }
+            };
+            int count=s.CountBattleships(board);
+        }
+    }
+}
---
+++ BattleShip_419/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShip_419
+{
+    public class Solution
+    {
+
+        public int CountBattleships(char[][] board)
+        {
+            int count = 0; int n = board.Length, m = board[0].Length;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (board[i][j] == 'X')
+                    {
+                        if((i>0 && board[i-1][j]=='X')||
+                            (j>0 && board[i][j - 1] == 'X'))
+                        {
+                            continue;
+                        }
+                        count++;                      
+                    }
+                }
+            }
+            return count;
+        }
+
+        private void dfs(char[][] board, int i, int j, int n, int m)
+        {
+            board[i][j] = '.';
+            if (i + 1 < n && board[i + 1][j] == 'X')
+                dfs(board, i + 1, j, n, m);
+
+        }
+        private void bfs(char[][] board, int i, int j, int n, int m)
+        {
+            board[i][j] = '.';
+            if (j + 1 < m && board[i][j + 1] == 'X')
+                bfs(board, i, j + 1, n, m);
+
+        }
+    }
+}
